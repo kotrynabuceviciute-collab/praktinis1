0dcbcf55258f4c298727e04a2e643f6bbc028f42
Kotryna Bucevičiūtė <kotryna.buceviciute@gmail.com>
2026-09-17 23:53:02 +0300

    new branch with a progress bar

--- app/src/main/java/com/example/praktinis1/MainActivity.java
+++ app/src/main/java/com/example/praktinis1/MainActivity.java
@@ -4,7 +4,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    boolean keitimas=false;
+    boolean keitimas = false;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView2);
         Button button = findViewById(R.id.button3);
         Button button2 = findViewById(R.id.button4);
+        progressBar = findViewById(R.id.progressBar);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,9 +41,9 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     textView.setText("Pirminis tekstas");
                 }
+                progress(5);
             }
         });
-
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,9 +53,19 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     textView.setTextColor(Color.BLUE);
                 }
-                keitimas=!keitimas;
+                keitimas = !keitimas;
+                progress(10);
             }
         });
+    }
 
+    private void progress(int amount) {
+        int dabartinis = progressBar.getProgress();
+        int naujas = dabartinis + amount;
+        if (naujas >= 100) {
+            naujas = 100;
+            Toast.makeText(this, "Pasiektas 100%", Toast.LENGTH_SHORT).show();
+        }
+        progressBar.setProgress(naujas);
     }
 }
