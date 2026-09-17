6df897322d2dfbba5d23f3fc26f068f9a461f95a
Kotryna Bucevičiūtė <kotryna.buceviciute@gmail.com>
2026-09-17 22:47:56 +0300

    Added button and TextView. When button is clicked, text changes.

--- app/src/main/java/com/example/praktinis1/MainActivity.java
+++ app/src/main/java/com/example/praktinis1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.praktinis1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView textView = findViewById(R.id.textView2);
+        Button button = findViewById(R.id.button3);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (textView.getText().toString().equals("Pirminis tekstas")) {
+                    textView.setText("Antrinis tekstas");
+                } else {
+                    textView.setText("Pirminis tekstas");
+                }
+            }
+        });
+
     }
 }
